1d23d868e5cccaebdca0caab05242d31a53187b2
Hexdee <lukmansaheed79@gmail.com>
2024-01-03 02:16:29 +0100

    Complete 0x15-api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
-"""getting data from an api
+"""
+Script to make request from an API
 """
 
-import requests
-from sys import argv
 
-if __name__ == '__main__':
-    endpoint = "https://jsonplaceholder.typicode.com"
-    userId = argv[1]
-    user = requests.get(endpoint + "users/{}".
-                        format(userId), verify=False).json()
-    todo = requests.get(endpoint + "todos?userId={}".
-                        format(userId), verify=False).json()
-    completed_tasks = []
-    for task in todo:
-        if task.get('completed') is True:
-            completed_tasks.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):".
-          format(user.get('name'), len(completed_tasks), len(todo)))
-    print("\n".join("\t {}".format(task) for task in completed_tasks))
+def counter(completed=None):
+    """Just to count completed task"""
+
+    ct = 0
+    for arg in todo:
+        if arg.get('completed') is True:
+            ct += 1
+    return ct
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    payload = {'id': argv[1]}
+    user = requests.get('https://jsonplaceholder.typicode.com/users',
+                        params=payload).json()
+
+    payload2 = {'userId': argv[1]}
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos',
+                        params=payload2).json()
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user[0].get('name'),
+        counter(todo),
+        len(todo)))
+
+    for arg in todo:
+        if arg.get('completed') is True:
+            print("\t {}".format(arg.get('title')))
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,22 +1,61 @@
 #!/usr/bin/python3
 """
-extend your Python script to export data in the CSV format
+Script to make request from an API
 """
 
-import csv
-import requests
-from sys import argv
 
-if __name__ == '__main__':
-    endpoint = "https://jsonplaceholder.typicode.com/"
-    userId = argv[1]
-    user = requests.get(endpoint + "users/{}".
-                        format(userId), verify=False).json()
-    todo = requests.get(endpoint + "todos?userId={}".
-                        format(userId), verify=False).json()
-    with open("{}.csv".format(userId), 'w', newline='') as csvfile:
-        my_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todo:
-            my_writer.writerow([int(userId), user.get('username'),
-                                task.get('completed'),
-                                task.get('title')])
+def counter(completed=None):
+    """Just to count completed task"""
+
+    ct = 0
+    for arg in todo:
+        if arg.get('completed') is True:
+            ct += 1
+    return ct
+
+
+def print_response(payload=None, payload2=None):
+    """print response object"""
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user[0].get('name'),
+        counter(todo),
+        len(todo)))
+
+    for arg in todo:
+        if arg.get('completed') is True:
+            print("\t {}".format(arg.get('title')))
+
+
+def to_csv(payload=None, payload2=None):
+    """create new CSV file and load"""
+
+    with open(argv[1] + ".csv", 'w') as csvfile:
+        fieldnames = ["USER_ID",
+                      "USERNAME",
+                      "TASK_COMPLETED_STATUS",
+                      "TASK_TITLE"]
+        writer = csv.DictWriter(csvfile,
+                                fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
+        for arg in todo:
+            writer.writerow({"USER_ID": arg.get('userId'),
+                             "USERNAME": user[0].get('username'),
+                             "TASK_COMPLETED_STATUS": arg.get('completed'),
+                             "TASK_TITLE": arg.get('title')})
+
+if __name__ == "__main__":
+    import requests
+    import csv
+    from sys import argv
+
+    payload = {'id': argv[1]}
+    user = requests.get('https://jsonplaceholder.typicode.com/users',
+                        params=payload).json()
+
+    payload2 = {'userId': argv[1]}
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos',
+                        params=payload2).json()
+
+    print_response(payload, payload2)
+    to_csv(payload, payload2)
